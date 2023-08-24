c3a08e6634ec0671ed84a4a76721aa75ebb6f91e
judy-chelangat <judysigilai75@gmail.com>
2023-08-24 14:41:46 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES
+
+('Mr. Chocolate' ,20 ,'M', 'blue', 'P', 0),
+('Rowdy', 23, 'F', 'PURPLE', 'N',1),
+('Tabitha' ,21 ,'M', 'YELLOW' ,'PI' ,0),
+('Sergeant Brown', 19 ,'F', 'blue', 'P', 1),
+('Melissa', 10, 'M', 'RED', 'PY', 1),
+('Grinch', 2, 'F', 'PINK', 'PU', 0),
+('Wendy', 17, 'F', 'blue', 'PN', 1),
+( NULL , 18 ,'M', 'pink' ,'PY', 1);
